cf756329dc36ef48b40880ce5882f99be3ef902f
Kaplantos <56962215+Kaplantos@users.noreply.github.com>
2019-10-24 15:23:49 +0200

    Update init-webchat.js

--- init-webchat.js
+++ init-webchat.js
@@ -2,5 +2,5 @@ setTimeout (function() {
 	initWebchat("https://endpoint-demo.cognigy.ai/fca954ab3c45e6b9d60d7f6c69845e8c198828f84324767723fd58a8a3139f82")
 		.then(cognigyWebchat => {
 		cognigyWebchat.open();
-	}); }, 5000);
+	}); }, 60000);
 
